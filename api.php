d0d90d3da0f17aecdb72ffb8079651d617e11c0a
Jonathan Ortega <jonortmor@gmail.com>
2025-02-08 20:11:59 +0100

    Segundo commit api.php con documentor añadido

--- api.php
+++ api.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * Busca información de un Pokémon utilizando la PokeAPI.
+ *
+ * Este script recibe el nombre de un Pokémon a través del parámetro GET 'name',
+ * realiza una solicitud a la PokeAPI y devuelve la información del Pokémon en formato JSON.
+ * 
+ * @category   PokemonAPI
+ * @package    PokeAPI
+ * @author     Tu Nombre <tu_email@ejemplo.com>
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link       https://pokeapi.co/
+ */
+
 if (isset($_GET['name'])) {
     $pokemonName = $_GET['name'];
     $url = "https://pokeapi.co/api/v2/pokemon/" . $pokemonName;
@@ -7,7 +20,8 @@ if (isset($_GET['name'])) {
         $data = json_decode($response, true);
         echo json_encode($data);
     } else {
-        echo json_encode(["error" => "Pokemon not found"]);
+        echo json_encode(["error" => "Pokémon no encontrado"]);
     }
 }
 ?>
+
